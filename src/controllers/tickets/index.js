78e674a57ab1fdc6c8e72b53ee5c7a4e14825070
Ryan Mazali <ryanmazali17@gmail.com>
2026-01-12 19:11:55 -0300

    Creating the route/query params

---
+++ src/controllers/tickets/index.js
@@ -0,0 +1,8 @@
+export function index(request, response, database) {
+    const { status } = request.query
+    console.log(status)
+
+    const tickets = database.select("tickets")
+    
+    return response.end(JSON.stringify(tickets))
+}
